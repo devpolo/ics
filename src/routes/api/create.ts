d5964433e041b84d68a3c7da79cfcee6fd601ab9
DevPolo <paul.crussaire@gmail.com>
2021-07-28 18:44:47 +0200

    ✅ FEAT: get exemple route

--- src/routes/api/create.ts
+++ src/routes/api/create.ts
@@ -12,7 +12,7 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
   }
 })
 
-router.post(
+router.get(
   "/string",
   async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -43,6 +43,50 @@ router.post(
         ...body,
         start: new Date(body.start),
         end: new Date(body.end),
+      }
+
+      const icalendar = new ICalendar(config)
+
+      res.setHeader("Content-type", "text/calendar")
+      res.charset = "UTF-8"
+      res.write(icalendar.render())
+      res.end()
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+
+router.get(
+  "/download/example",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const body: CalendarOptions = req.body
+
+      const config = {
+        ...body,
+        title: "Welcome at SCEP",
+        location: "SCEP",
+        description: "You are welcome",
+        start: new Date("2024-01-01"),
+        end: new Date("2024-01-01"),
+        attendees: [
+          {
+            name: "Paul Crussaire",
+            email: "pcrussaire@outlook.com",
+            icsOptions: {
+              role: "CHAIR",
+              rsvp: true,
+            },
+          },
+          {
+            name: "Alex Beruet",
+            email: "alex@myve.live",
+            icsOptions: {
+              role: "REQ-PARTICIPANT",
+            },
+          },
+        ],
       }
 
       const icalendar = new ICalendar(config)
