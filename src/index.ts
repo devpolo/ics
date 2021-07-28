0f2c6f78c516debfb42311e6be36b66f14e2f3f7
DevPolo <paul.crussaire@gmail.com>
2021-07-28 17:11:38 +0200

    🐛 BUG FIX: port on production

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express"
 
 const app = express()
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get("/", (_, res) => {
   res.status(200).send({ message: "OK" })
