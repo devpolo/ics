241f55722f0162d885c04757e472fd0799f40639
DevPolo <paul.crussaire@gmail.com>
2021-07-28 16:31:45 +0200

    ✅ FEAT: message on get route

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ const app = express()
 const port = 5000
 
 app.get("/", (_, res) => {
-  res.status(200).send()
+  res.status(200).send({ message: "OK" })
 })
 app.listen(port, () => console.log(`Running on port ${port}`))
