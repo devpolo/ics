87b1436cacc130a36ad90a3aaa725d007e7f9209
DevPolo <paul.crussaire@gmail.com>
2021-07-28 18:31:06 +0200

    ✅ FEAT: create

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express"
 
+import { CalendarOptions, ICalendar } from "datebook"
+
 const router = express.Router()
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
@@ -9,5 +11,26 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     next(error)
   }
 })
+
+router.post(
+  "/create",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const body: CalendarOptions = req.body
+
+      const config = {
+        ...body,
+        start: new Date(body.start),
+        end: new Date(body.end),
+      }
+
+      const icalendar = new ICalendar(config)
+
+      res.status(200).json(icalendar.render())
+    } catch (error) {
+      next(error)
+    }
+  }
+)
 
 module.exports = router
