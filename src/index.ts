d0b9a9adf41a872249034cde879578c122250c26
DevPolo <paul.crussaire@gmail.com>
2021-07-29 15:45:53 +0200

    ✅ FEAT: email routes

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/create", require("./routes/api/create"))
+app.use("/email", require("./routes/api/email"))
 
 app.use(handleErrors)
 
